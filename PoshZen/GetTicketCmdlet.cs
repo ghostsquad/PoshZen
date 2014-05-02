75f6ea9732cab6998f31f10621ce6f07ba743dff
Weston McNamee <ghost.squadron@gmail.com>
2014-05-02 14:52:39 -0700

    initial commit starting foundations

---
+++ PoshZen/GetTicketCmdlet.cs
@@ -0,0 +1,29 @@
+﻿namespace PoshZen
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Management.Automation;
+    using System.Text;
+    using System.Threading.Tasks;
+    using SharpZendeskApi.Core;
+
+    [Cmdlet("Get", "Ticket")]
+    public class GetTicketCmdlet : Cmdlet
+    {
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();            
+        }
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+        }
+    }
+}
