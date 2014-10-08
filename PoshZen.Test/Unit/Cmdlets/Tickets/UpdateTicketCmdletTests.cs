574f784a5bc8a0ca45b963fd73e44f14cca87318
Weston McNamee <ghost.squadron@gmail.com>
2014-10-08 10:35:36 -0700

    adding Update-Ticket cmdlet

---
+++ PoshZen.Test/Unit/Cmdlets/Tickets/UpdateTicketCmdletTests.cs
@@ -0,0 +1,43 @@
+﻿namespace PoshZen.Test.Unit.Cmdlets.Tickets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Management.Automation;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using FluentAssertions;
+
+    using Moq;
+
+    using Ploeh.AutoFixture;
+
+    using SharpZendeskApi.Management;
+    using SharpZendeskApi.Models;
+
+    using Xunit;
+
+    public class UpdateTicketCmdletTests : CmdletTestBase<Ticket, ITicket, ITicketManager>
+    {
+        [Fact]
+        public void UpdateTicket_ExpectSubmitUpdatesCalledWithProvidedITicket()
+        {
+            var ticketInput = new Ticket(1, "foo");
+
+            this.ManagerMock.Setup(x => x.SubmitUpdatesFor(It.IsAny<ITicket>()))
+                .Verifiable();
+
+            var psCommand = new PSCommand();
+            psCommand.AddCommand("Update-Ticket");
+            psCommand.AddArgument(ticketInput);
+
+            // act
+            var invocationData = this.Invoke(psCommand);
+
+            invocationData.Results.Should().BeEmpty();
+            invocationData.ErrorRecords.Should().BeEmpty();
+            this.ManagerMock.Verify(x => x.SubmitUpdatesFor(ticketInput), Times.Once());
+        }                
+    }
+}
---
+++ PoshZen/Cmdlets/Tickets/UpdateTicketCmdlet.cs
@@ -0,0 +1,32 @@
+﻿namespace PoshZen.Cmdlets.Tickets
+{
+    using System.Management.Automation;
+
+    using SharpZendeskApi;
+    using SharpZendeskApi.Management;
+    using SharpZendeskApi.Models;
+
+    [Cmdlet("Update", CmdletNamingConstants.Ticket, DefaultParameterSetName = ParamSetDefault)]
+    public class UpdateTicketCmdlet : PoshZenCmdletBase<ITicket, ITicketManager>
+    {
+        private const string ParamSetDefault = "default";
+
+        [Parameter(Position = 1)]
+        public override ZendeskClientBase Client { get; set; }
+
+        [Parameter(Position = 0, ValueFromPipeline = true, ParameterSetName = ParamSetDefault, Mandatory = true)]       
+        public ITicket Ticket { get; set; }
+
+        #region Methods        
+
+        protected override void BeginProcessing() {            
+            this.ResolveManager();
+        }
+
+        protected override void ProcessRecord() {
+            this.Manager.SubmitUpdatesFor(this.Ticket);
+        }
+
+        #endregion
+    }
+}
